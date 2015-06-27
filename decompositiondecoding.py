4ff9e92071fe01398e7b20223b554c6e545ca7ee
poteryu <ccmmelati@gmail.com>
2015-06-27 13:46:18 +0900

    added code-hungry

--- decompositiondecoding.py
+++ decompositiondecoding.py
@@ -1,4 +1,42 @@
-def triangle_mat_decompo(input_mat):
-	triangle_mat=[]
+
+#row operation functions, xor and exchange
+def row_ops (matrix, source_i, des_i, mode): 
 	
-	for pivot in range(len(input_mat))
+	if mode == 'ex': #exchange mode, swaps row
+		row_op_temp = []
+		row_op_temp = matrix[source_i]
+		matrix[des_i] = matrix[source_i]
+		matrix[source_i] = row_op_temp
+		
+		return matrix
+		
+	else if mode == 'xor' #xor operation, des=des xor source
+		matrix[des_i] = matrix[des_i].__xor__(input_mat[pivot])
+		
+		return matrix
+	
+def triangle_mat_decompo(input_mat):
+		
+	for pivot in range(len(input_mat[0])):	#pivot at [value] column
+		
+		# identify rows with pivot at [pivot] column
+		pivot_rows = []
+		for i in range(pivot, len(input_mat)):
+			if input_mat[i][pivot] == 1:
+				pivot_rows.append(i)
+						
+		#putting the first pivot rows to the top
+		if pivot_rows[0] != pivot
+			row_ops(input_mat, pivot, pivot_rows[0], 'ex')
+			del pivot_rows[0]
+		
+		#clearing values in [pivot] column except pivot row
+		for i in pivot_rows:
+			row_ops(input_mat, i, pivot, 'xor')
+			del pivot_rows[i]
+		
+	return input_mat	
+		
+def solve_triangular_matrix(input_mat):
+	
+	
