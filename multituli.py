64ec79e1da3681ed9a3185b6eba63e9b439c3f73
poteryu <ccmmelati@gmail.com>
2015-08-09 23:08:27 +0800

    00

---
+++ multituli.py
@@ -0,0 +1,21 @@
+def FormPartialUpperTriangularMatrix(partialMat,m):
+	resultMat=[]
+	for i in range(m-1):
+		#search for 1 in the 1st column, break on 1st encounter, cut row from partial mat to result mat.
+		#if not pivot not found, set row to zero, add rowIndex into errList, continue for loop
+		#perform row opeartion to clear out column
+		#rinse and repeat!
+	if errList != []: #if errlist not empty
+		#replace row in errList with leftover row in partial mat
+
+	return resultMat
+
+def MergingPartialUpperTriangularMatrix(partialMatTop,partialMatBtm):
+	for i in range(m-1):
+		if partialMatTop[i][i]==0:
+			#assert partialMatBtm[i][i] ==1
+			#swap row [i]
+		for j in range(m-1):
+			if partialMatBtm#ith entry jth row, is non-zero
+				#xor
+				#xor
